e92ba1f6e1187a4ec3245c7b218439330551bf9c
Nurutomo26 <jayaxyz16@gmail.com>
2022-05-29 05:29:12 +0800

    d

---
+++ plugins/donasi.js
@@ -0,0 +1,16 @@
+let handler =  m => m.reply(`
+╭─「 Donasi • Pulsa 」
+│ • 3 [089524664142]
+│ • Telkomsel [082339922441]
+╰────
+
+╭─「 Donasi • Non Pulsa 」
+│ • Dana [089524664142]
+│ • Gopay [089524664142]
+╰────
+`.trim()) // Tambah sendiri kalo mau
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
+
+export default handler
