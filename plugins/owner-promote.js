eca52c6cca4f88bf292a3fc69014ac724473fced
Nurutomo26 <103919858+Nurutomo26@users.noreply.github.com>
2022-05-30 18:47:36 +0800

    Update owner-promote.js

--- plugins/owner-promote.js
+++ plugins/owner-promote.js
@@ -1,17 +1,26 @@
+import { areJidsSameUser } from '@adiwajshing/baileys'
 let handler = async (m, { conn, participants }) => {
-  let members = participants.filter(member => !member.isAdmin).map(member => member.jid)
-  let users = m.mentionedJid.filter(user => members.includes(user))
-  for (let user of users) await conn.groupParticipantsUpdate(m.chat, [user], 'promote').catch(console.log)
+    let users = m.mentionedJid.filter(u => !areJidsSameUser(u, conn.user.id))
+    let promoteUser = []
+    for (let user of users)
+        if (user.endsWith('@s.whatsapp.net') && !(participants.find(v => areJidsSameUser(v.id, user)) || { admin: true }).admin) {
+            const res = await conn.groupParticipantsUpdate(m.chat, [user], 'promote')
+            promoteUser.concat(res)
+            await delay(1 * 1000)
+        }
+    m.reply(`Succes promote ${promoteUser.map(v => '@' + v.split('@')[0])}`, null, { mentions: promoteUser })
+
 }
 handler.help = ['promote','admin','^'].map(v => 'o' + v + ' @user')
-handler.tags = ['owner']
+handler.tags = ['group']
 
-handler.command = /^(opromote|oadmin|o\^)$/i
+handler.command = /^(opromote|oadmin|\o^)$/i
 
-handler.owner = true
 handler.group = true
 
-handler.admin = false
+handler.owner = true
 handler.botAdmin = true
 
 export default handler
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
