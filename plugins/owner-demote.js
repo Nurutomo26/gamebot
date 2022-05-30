d9b509d8152d0e449084c9374bccbe98314a6a8d
Nurutomo26 <103919858+Nurutomo26@users.noreply.github.com>
2022-05-30 18:42:10 +0800

    Update owner-demote.js

--- plugins/owner-demote.js
+++ plugins/owner-demote.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, participants }) => {
   for (let user of users) await conn.groupParticipantsUpdate(m.chat, [user], 'demote').catch(console.log)
 }
 handler.help = ['demote','member','↓'].map(v => v + ' @user')
-handler.tags = ['admin']
+handler.tags = ['owner']
 
 handler.command = /^(demote|member|↓)$/i
 
