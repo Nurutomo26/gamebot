df41422cce3103ea64e676c6bd46171487d2d135
Nurutomo26 <103919858+Nurutomo26@users.noreply.github.com>
2022-05-30 17:36:55 +0800

    Add files via upload

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,13 +1,16 @@
-import { tiktokdl, tiktokdlv2 } from '@bochilteam/scraper'
+import fetch from 'node-fetch'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
-    const { author: { nickname }, video, description } = await tiktokdl(args[0]).catch(async _ => await tiktokdlv2(args[0]))
-    const url = video.no_watermark_raw || video.no_watermark || video.no_watermark_hd || video.with_watermark 
-    if (!url) throw 'Can\'t download video!'
-    conn.sendFile(m.chat, url, 'tiktok.mp4', `
-🔗 *Url:* ${url}
-🧏 *Nickname:* ${nickname}${description ? `🖹 *Description:* ${description}` : ''}
-`.trim(), m)
+  if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
+  let res = await fetch(global.API('xteam', '/dl/tiktok', {
+    url: args[0]
+  }, 'APIKEY'))
+  if (res.status !== 200) throw await res.text()
+  let json = await res.json()
+  if (!json.status) throw json
+  let url = json.result.link_dl1 || json.result.link_dl2 || ''
+  if (!url) throw 'Gagal mengambil url download'
+  let txt = `- *By:* ${json.result.name}\n- *Caption:*\n${json.result.caption}`
+  await conn.sendFile(m.chat, url, 'tiktok.mp4', txt.trim(), m)
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
