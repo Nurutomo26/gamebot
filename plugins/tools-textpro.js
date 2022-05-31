ca74bf325833aa716a498aa45963fbee2c8e0747
Nurutomo26 <103919858+Nurutomo26@users.noreply.github.com>
2022-05-31 11:15:41 +0800

    Add files via upload

--- plugins/tools-textpro.js
+++ plugins/tools-textpro.js
@@ -1,8 +1,8 @@
 import fetch from 'node-fetch'
 let split = '|'
 let handler = async (m, { conn, args: [effect], text: txt }) => {
-  let { effects } = await (await (fetch(global.API('xteam', '/textpro')))).json()
-  if (!effect) throw '*Daftar Efek*\n\n' + effects.sort((a, b) => a - b).join('\n')
+  let { effects } = await fetch(global.API('xteam', '/textpro', {}, 'APIKEY'))
+  if (!effect) throw '*Daftar Efek*\n\n' + effects.effects((a, b) => a - b) + '\n\n' + effects.effects_2((a, b) => a -b).join('\n')
   effect = effect.toLowerCase()
   if (!effect in effects) throw `Efek *${effect}* tidak ditemukan`
   let [text, text2, ...text3] = txt.replace(effect, '').trimStart().split(split)
