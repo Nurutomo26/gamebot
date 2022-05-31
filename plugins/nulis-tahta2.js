1bd3a54154cee474ce11b2857d9da7369b48596f
Nurutomo26 <103919858+Nurutomo26@users.noreply.github.com>
2022-05-31 17:45:46 +0800

    Update nulis-tahta2.js

--- plugins/nulis-tahta2.js
+++ plugins/nulis-tahta2.js
@@ -9,19 +9,11 @@ let handler  = async (m, { conn, text }) => {
  text,
 await conn.sendFile(m.chat, global.API('xteam', '/tahta', { text, }, 'APIKEY'), 'tahta.png', 'Nih udah jadi harta tahtamu...', m)
 }
-handler.help = ['tahta2'].map(v => v + '<teks>')
+handler.help = ['tahta2'].map(v => v + ' <teks>')
 handler.tags = ['nulis']
+
 handler.command = /^tahta2$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = true
-handler.private = false
-
-handler.admin = false
-handler.botAdmin = false
-
 handler.fail = null
-handler.register = true
+handler.limit = true
 
 export default handler
