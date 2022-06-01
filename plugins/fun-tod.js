4c14936e87631a316e2c5bd90042b1c0c99491d8
Nurutomo26 <103919858+Nurutomo26@users.noreply.github.com>
2022-06-01 08:11:10 +0800

    Add files via upload

--- plugins/fun-tod.js
+++ plugins/fun-tod.js
@@ -1,5 +1,6 @@
 let handler = async(m, { conn }) => {
-	conn.sendButton(m.chat, '*Truth Or Dare*', author, [['Truth', '/truth'], ['Dare', '/dare']], m)
+	let img = 'https://i.ibb.co/305yt26/bf84f20635dedd5dde31e7e5b6983ae9.jpg'
+	conn.sendButton(m.chat, '*Truth Or Dare*', author, img, [['Truth', '/truth'], ['Dare', '/dare']], m)
 }
 handler.help = ['tod', 'truthordare']
 handler.tags = ['fun']
